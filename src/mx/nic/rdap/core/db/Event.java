817f50738fa984850b0f1d209e8937978b15443f
dalpuche <alpuche1990@gmail.com>
2016-09-13 15:28:02 -0500

    Rewrite class comments

--- src/mx/nic/rdap/core/db/Event.java
+++ src/mx/nic/rdap/core/db/Event.java
@@ -1,12 +1,12 @@
 package mx.nic.rdap.core.db;
 
 /**
- * POJO for the Event object
+ * POJO for the Event object.This data structure represents events that have
+ * occurred on an instance of an object class
  * 
  * @author dalpuche
  *
  */
-public class Event  {
-
+public class Event {
 
 }
--- src/mx/nic/rdap/core/db/Link.java
+++ src/mx/nic/rdap/core/db/Link.java
@@ -1,13 +1,12 @@
 package mx.nic.rdap.core.db;
 
 /**
- * POJO for the Link object
+ * POJO for the Link object.The object is a data structure that signify link an
+ * object to other resources on the Internet.
  * 
  * @author dalpuche
  *
  */
 public class Link {
-
-	
 
 }
