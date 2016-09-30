b3f7485e923755f7dd00221803f4c87308ba37a4
dhfelix <daniel.hdz.felix@hotmail.com>
2016-09-29 20:37:36 -0500

    Added and remove some attributes.

--- src/mx/nic/rdap/core/db/Variant.java
+++ src/mx/nic/rdap/core/db/Variant.java
@@ -1,5 +1,6 @@
 package mx.nic.rdap.core.db;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import mx.nix.rdap.core.catalog.VariantRelation;
@@ -37,6 +38,60 @@ public class Variant {
 	 * An array of VariantRelation objects {@link VariantRelation}
 	 */
 	private List<VariantRelation> relations;
+
+	public Variant() {
+		variantNames = new ArrayList<>();
+		relations = new ArrayList<>();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((domainId == null) ? 0 : domainId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((idnTable == null) ? 0 : idnTable.hashCode());
+		result = prime * result + ((relations == null) ? 0 : relations.hashCode());
+		result = prime * result + ((variantNames == null) ? 0 : variantNames.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Variant))
+			return false;
+		Variant other = (Variant) obj;
+		if (domainId == null) {
+			if (other.domainId != null)
+				return false;
+		} else if (!domainId.equals(other.domainId))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (idnTable == null) {
+			if (other.idnTable != null)
+				return false;
+		} else if (!idnTable.equals(other.idnTable))
+			return false;
+		if (relations == null) {
+			if (other.relations != null)
+				return false;
+		} else if (relations.size() != other.relations.size() || !relations.containsAll(other.relations))// !relations.equals(other.relations))
+			return false;
+		if (variantNames == null) {
+			if (other.variantNames != null)
+				return false;
+		} else if (variantNames.size() != other.variantNames.size() || !variantNames.containsAll(other.variantNames))// !variantNames.equals(other.variantNames))
+			return false;
+		return true;
+	}
 
 	/**
 	 * @return the id
