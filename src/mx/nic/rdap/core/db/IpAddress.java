9230d3a06db4e8ea281ebe753b2f64c5a6796056
dhfelix <daniel.hdz.felix@hotmail.com>
2016-10-04 15:52:10 -0500

    Add Equals and hash functions

--- src/mx/nic/rdap/core/db/IpAddress.java
+++ src/mx/nic/rdap/core/db/IpAddress.java
@@ -36,6 +36,40 @@ public class IpAddress {
 
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + (int) (nameserverId ^ (nameserverId >>> 32));
+		result = prime * result + type;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof IpAddress))
+			return false;
+		IpAddress other = (IpAddress) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nameserverId != other.nameserverId)
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
 	/**
 	 * @return the nameserverId
 	 */
