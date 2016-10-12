e00006281aa2a3c39f6c6b8ed911dc3fc7b6da0b
Zakrom <edgararturo@msn.com>
2016-10-12 12:39:55 -0500

    Add getByName in VariantRelation

--- src/mx/nix/rdap/core/catalog/VariantRelation.java
+++ src/mx/nix/rdap/core/catalog/VariantRelation.java
@@ -9,13 +9,15 @@ package mx.nix.rdap.core.catalog;
  *
  */
 public enum VariantRelation {
-	
-	REGISTERED(1, "registered", "The variant names are registered in the registry."),
-	UNREGISTERED(2, "unregistered", "The variant names are not found in the registry."),
-	REGISTRATION_RESTRICTED(3, "registration restricted", "Registration of the variant names is restricted to certain parties or within certain rules."),
-	OPEN_REGISTRATION(4, "open registration", "Registration of the variant names is available to generally qualified registrants."),
-	CONJOINED(5, "conjoined", "Registration of the variant names occurs automatically with the registration of the containing domain registration."),
-	UNKNOWN(0, null, null);
+
+	REGISTERED(1, "registered", "The variant names are registered in the registry."), UNREGISTERED(2, "unregistered",
+			"The variant names are not found in the registry."), REGISTRATION_RESTRICTED(3, "registration restricted",
+					"Registration of the variant names is restricted to certain parties or within certain rules."), OPEN_REGISTRATION(
+							4, "open registration",
+							"Registration of the variant names is available to generally qualified registrants."), CONJOINED(
+									5, "conjoined",
+									"Registration of the variant names occurs automatically with the registration of the containing domain registration."), UNKNOWN(
+											0, null, null);
 
 	/**
 	 * An unique identifier for the variant relation
@@ -62,6 +64,29 @@ public enum VariantRelation {
 		case 4:
 			return VariantRelation.OPEN_REGISTRATION;
 		case 5:
+			return VariantRelation.CONJOINED;
+		default:
+			return VariantRelation.UNKNOWN;
+		}
+	}
+
+	/**
+	 * Get relation from a String of a variant
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static VariantRelation getByName(String name) {
+		switch (name) {
+		case "registered":
+			return VariantRelation.REGISTERED;
+		case "unregistered":
+			return VariantRelation.UNREGISTERED;
+		case "registration restricted":
+			return VariantRelation.REGISTRATION_RESTRICTED;
+		case "open registration":
+			return VariantRelation.OPEN_REGISTRATION;
+		case "conjoined":
 			return VariantRelation.CONJOINED;
 		default:
 			return VariantRelation.UNKNOWN;
